54d11c66f934eccfa683cc9379f276914606aaf4
AAAA <dev@onerbs.com>
2020-07-20 21:22:43 -0500

    Add Swagger and JWT

--- school/urls.py
+++ school/urls.py
@@ -13,18 +13,27 @@ Including another URLconf
 	1. Import the include() function: from django.urls import include, path
 	2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf.urls import url
 from django.contrib import admin
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_swagger.views import get_swagger_view
 
 from courses.views import CourseViewSet
+from students.views import StudentViewSet
 
 router = DefaultRouter()
-router.register(r'courses', CourseViewSet)
+router.register('courses', CourseViewSet)
+router.register('students', StudentViewSet)
+
+schema_view = get_swagger_view(title='API')
 
 urlpatterns = [
 	path('admin/', admin.site.urls),
-	url(r'^api/v1/', include(router.urls)),
-	url(r'^api-auth/', include('rest_framework.urls')),
+	path('api/v1/', include(router.urls)),
+	path('api-auth/', include('rest_framework.urls')),
+	path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+	path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+	# path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
+	path('docs/', schema_view)
 ]
